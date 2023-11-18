7d8fc0124ce7067cb8d00ee69bffd7ff1ecb217e
Javier <francisco.javier.garrido@hotmail.com>
2023-11-18 23:14:24 +0100

    Creado método crearReyDefecto

--- src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
@@ -7,6 +7,7 @@ import static org.iesalandalus.programacion.reyajedrez.Consola.*;
 public class MainApp {
 
     private static Rey rey;
+
     public static void main(String[] args) {
 
         int opcion;
@@ -14,14 +15,35 @@ public class MainApp {
         do {
             mostrarMenu();
             opcion = elegirOpcionMenu();
-
             ejecutarOpcion(opcion);
 
         } while (opcion != 4);
     }
 
     private static void ejecutarOpcion(int opcion) {
+        switch (opcion) {
+            case 1:
+                crearReyDefecto();
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            default:
+                System.out.println("Opción no válida. Introduce un número del 1 al 5.");
+                break;
+        }
+
+
     }
 
 
+    private static void crearReyDefecto() {
+        rey = new Rey(); // Esto asume que la clase Rey tiene un constructor por defecto
+        System.out.println("Rey creado por defecto.");
+    }
 }
