1bdab119dc9b9370f9f2c84b3c4b2fa282075115
Javier <francisco.javier.garrido@hotmail.com>
2023-11-18 19:24:24 +0100

    Creado menú

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
@@ -7,4 +7,14 @@ public class Consola {
 
     }
 
+    public static void mostrarMenu(){
+        System.out.println("   ********   Menu   ********");
+        System.out.println("==================================");
+        System.out.println("| 1. Crear rey por defecto       |");
+        System.out.println("| 2. Crear rey eligiendo color   |");
+        System.out.println("| 3. Mover                       |");
+        System.out.println("| 4. Salir                       |");
+        System.out.println("----------------------------------");
+    }
+
 }
