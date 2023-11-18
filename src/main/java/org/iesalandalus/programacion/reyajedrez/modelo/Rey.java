f3156dcc9d5b0c0f062bdef467a6396cd305979e
Javier <francisco.javier.garrido@hotmail.com>
2023-11-18 19:08:10 +0100

    Creado método mover

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.reyajedrez.modelo;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Rey {
 
     private Color color;
@@ -67,4 +69,70 @@ public class Rey {
         this.color=color;
         this.totalMovimientos=0;
     }
+
+
+    public void mover(Direccion direccion) throws OperationNotSupportedException {
+        //comprueba si es nulo
+        if (direccion==null){
+            throw new NullPointerException("La dirección no puede ser nula");
+        }
+
+        //si Rey está en su posición puede enrocarse
+        if (totalMovimientos==0 && (direccion==Direccion.ENROQUE_CORTO || direccion==Direccion.ENROQUE_LARGO)){ //puede enrocarse
+
+            char nuevaColumna;
+            if (direccion == Direccion.ENROQUE_CORTO){
+                nuevaColumna=(char)(posicion.getColumna()+2);  //enroque corto 2 casillas derecha
+
+            }else {  //(direccion == Direccion.ENROQUE_LARGO)
+                nuevaColumna = (char)(posicion.getColumna()-2); //enroque largo 2 casillas izquierda
+            }
+            this.posicion.setColumna(nuevaColumna); //actualizamos la posición del Rey
+            totalMovimientos++;
+
+
+        }else{ //no puede enrocarse, porque ya ha movido el Rey, así que puede hacer movimiento normal
+
+            Posicion nuevaPosicion=null;
+            switch (direccion){
+                case NORTE:
+                    nuevaPosicion= new Posicion(posicion.getFila()+1, posicion.getColumna());
+                    break;
+                case NORESTE:
+                    nuevaPosicion= new Posicion(posicion.getFila()+1, (char) (posicion.getColumna()+1));
+                    break;
+                case ESTE:
+                    nuevaPosicion= new Posicion(posicion.getFila(), (char) (posicion.getColumna()+1));
+                    break;
+                case SURESTE:
+                    nuevaPosicion= new Posicion(posicion.getFila()-1, (char) (posicion.getColumna()+1));
+                    break;
+                case SUR:
+                    nuevaPosicion= new Posicion(posicion.getFila()-1,posicion.getColumna());
+                    break;
+                case SUROESTE:
+                    nuevaPosicion= new Posicion(posicion.getFila()-1, (char) (posicion.getColumna()-1));
+                    break;
+                case OESTE:
+                    nuevaPosicion= new Posicion(posicion.getFila(), (char) (posicion.getColumna()-1));
+                    break;
+                case NOROESTE:
+                    nuevaPosicion= new Posicion(posicion.getFila()+1, (char) (posicion.getColumna()-1));
+                    break;
+                default:
+                    throw new IllegalArgumentException("Direcion no permitida.");
+            }
+
+            //verificamos que esté dentro del tablero
+            if ((nuevaPosicion.getFila() >=1 && nuevaPosicion.getFila()<=8) &&
+                    (nuevaPosicion.getColumna()>='a' && nuevaPosicion.getColumna()<='h')){
+
+                this.posicion=nuevaPosicion;
+                totalMovimientos++;
+            }else {
+                throw new OperationNotSupportedException("Movimiento fuera del tablero");
+            }
+        }
+    }
+
 }
