293c9d84af81d641aa240550f11bed70e5ffc329
Javier <francisco.javier.garrido@hotmail.com>
2023-11-18 22:06:25 +0100

    Creado método ejecutarOpcion

--- src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
@@ -1,7 +1,22 @@
 package org.iesalandalus.programacion.reyajedrez;
 
+import org.iesalandalus.programacion.reyajedrez.modelo.*;
+
 public class MainApp {
+
+    private static Rey rey;
     public static void main(String[] args) {
 
+        int opcion;
+
+        do {
+            mostrarMenu();
+            opcion = elegirOpcionMenu();
+
+            ejecutarOpcion(opcion);
+
+        } while (opcion != 4);
     }
+
+
 }
