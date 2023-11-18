ccea86ffca2f9ad56bf8ab6192b1f1a40b5a36ce
Javier <francisco.javier.garrido@hotmail.com>
2023-11-18 13:22:40 +0100

    Creado Métodos get y set para los 3 atributos

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
@@ -6,5 +6,46 @@ public class Rey {
     private Posicion posicion;
     private int totalMovimientos;
 
+    //Método getColor
+    public Color getColor() {
+        return color;
+    }
 
+    //Método setColor
+    public void setColor(Color color) {
+        if (color==null){
+            throw new NullPointerException("El color no puede ser nulo.");
+        }
+        this.color = color;
+    }
+
+
+
+    //Método get Posicion
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    //Método set Posicion
+    public void setPosicion(Posicion posicion) {
+        if (posicion==null){
+            throw new NullPointerException("La posición no puede ser nula.")
+        }
+        this.posicion = posicion;
+    }
+
+
+
+    //Método get totalMovimientos
+    public int getTotalMovimientos() {
+        return totalMovimientos;
+    }
+
+    //Método set totalMovimientos
+    public void setTotalMovimientos(int totalMovimientos) {
+        if (totalMovimientos<0){
+            throw IllegalArgumentException("Tiene que haber algún movimiento");
+        }
+        this.totalMovimientos = totalMovimientos;
+    }
 }
